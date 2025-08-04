f98cae24e901fb2bad4fc1971376d1e55342fe47
Alex Hedley <79629950+alex-hedley@users.noreply.github.com>
2025-08-04 17:47:01 +0100

    Update UI to parse hardcoded value and print

--- src/composeApp/src/wasmJsMain/kotlin/com/alexhedley/App.kt
+++ src/composeApp/src/wasmJsMain/kotlin/com/alexhedley/App.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Label
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,15 +23,14 @@ import utility_kotlin.composeapp.generated.resources.compose_multiplatform
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
+        var durationText by remember { mutableStateOf("PT10M14.230852287S") }
         Column(
             modifier = Modifier
                 .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
+
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
@@ -37,6 +38,30 @@ fun App() {
                     Text("Compose: $greeting")
                 }
             }
+            // Label
+            Text("PT10M14.230852287S")
+
+            // Duration input field
+            TextField(
+                "PT10M14.230852287S",
+                onValueChange = {
+//                    val duration = DurationParser().parse(it)
+//                    println(duration)
+                    durationText = it
+                },
+                placeholder = { Text("Duration") }
+            )
+
+            Button(onClick = {
+                showContent = !showContent
+                println("Duration: '$durationText'")
+                val duration = DurationParser().parse(durationText)
+            }) {
+                Text("Click me!")
+            }
+
+
+
         }
     }
 }
