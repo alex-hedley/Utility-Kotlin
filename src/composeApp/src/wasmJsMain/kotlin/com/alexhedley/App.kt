9deeab2a730d4fb001141ad4d7d8ca6cd77e8ab2
Alex Hedley <79629950+alex-hedley@users.noreply.github.com>
2025-08-05 14:42:03 +0100

    1 file wonder

--- src/composeApp/src/wasmJsMain/kotlin/com/alexhedley/App.kt
+++ src/composeApp/src/wasmJsMain/kotlin/com/alexhedley/App.kt
@@ -14,7 +14,10 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import org.jetbrains.compose.resources.painterResource
+
+import kotlin.time.Duration;
 
 import utility_kotlin.composeapp.generated.resources.Res
 import utility_kotlin.composeapp.generated.resources.compose_multiplatform
@@ -22,8 +25,17 @@ import utility_kotlin.composeapp.generated.resources.compose_multiplatform
 @Composable
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
         var durationText by remember { mutableStateOf("PT10M14.230852287S") }
+
+        var textFieldDaysValue by remember { mutableStateOf("") }
+        var textFieldHoursValue by remember { mutableStateOf("") }
+        var textFieldMinutesValue by remember { mutableStateOf("") }
+        var textFieldMillisValue by remember { mutableStateOf("") }
+        var textFieldNanosValue by remember { mutableStateOf("") }
+        var textFieldSecondsValue by remember { mutableStateOf("") }
+
+        var textErrorValue by remember { mutableStateOf("") }
+
         Column(
             modifier = Modifier
                 .safeContentPadding()
@@ -31,36 +43,85 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
 
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-            // Label
-            Text("PT10M14.230852287S")
+            Text("Java / Kotlin Duration parser:", style = MaterialTheme.typography.titleLarge)
+
+            Text("Example: PT10M14.230852287S")
 
             // Duration input field
             TextField(
-                "PT10M14.230852287S",
-                onValueChange = {
-//                    val duration = DurationParser().parse(it)
-//                    println(duration)
-                    durationText = it
-                },
-                placeholder = { Text("Duration") }
+                durationText,
+                onValueChange = { durationText = it },
+                placeholder = { Text("Duration") },
+                label = { Text("Duration") }
             )
 
+            Text(textErrorValue, color = Color.Red)
+
             Button(onClick = {
-                showContent = !showContent
                 println("Duration: '$durationText'")
-                val duration = DurationParser().parse(durationText)
+//                val duration = DurationParser().parse(durationText)
+                try {
+                    val duration = Duration.parse(durationText);
+
+                    println("Days       : " + duration.inWholeDays)
+                    textFieldDaysValue = duration.inWholeDays.toString()
+                    println("Hours      : " + duration.inWholeHours)
+                    textFieldHoursValue = duration.inWholeHours.toString()
+                    println("Minutes    : " + duration.inWholeMinutes)
+                    textFieldMinutesValue = duration.inWholeMinutes.toString()
+                    println("Seconds    : " + duration.inWholeSeconds)
+                    textFieldSecondsValue = duration.inWholeSeconds.toString()
+                    println("Millis     : " + duration.inWholeMilliseconds)
+                    textFieldMillisValue = duration.inWholeMilliseconds.toString()
+                    println("Nanos      : " + duration.inWholeNanoseconds)
+                    textFieldNanosValue = duration.inWholeNanoseconds.toString()
+
+                    textErrorValue = ""
+                }
+                catch (e:Exception) {
+                    println("Error: $e")
+                    textErrorValue = e.message.toString()
+                }
+
             }) {
-                Text("Click me!")
+                Text("Convert")
             }
 
+            TextField(textFieldDaysValue,
+                onValueChange = { textFieldDaysValue = it },
+                placeholder = { Text("Days") },
+                label = { Text("Days") }
+            )
 
+            TextField(textFieldHoursValue,
+                onValueChange = { textFieldHoursValue = it },
+                placeholder = { Text("Hours") },
+                label = { Text("Hours") }
+            )
+
+            TextField(textFieldMinutesValue,
+                onValueChange = { textFieldMinutesValue = it },
+                placeholder = { Text("Minutes") },
+                label = { Text("Minutes") }
+            )
+
+            TextField(textFieldSecondsValue,
+                onValueChange = { textFieldSecondsValue = it },
+                placeholder = { Text("Seconds") },
+                label = { Text("Seconds") }
+            )
+
+            TextField(textFieldMillisValue,
+                onValueChange = { textFieldMillisValue = it },
+                placeholder = { Text("Millis") },
+                label = { Text("Millis") }
+            )
+
+            TextField(textFieldNanosValue,
+                onValueChange = { textFieldNanosValue = it },
+                placeholder = { Text("Nanos") },
+                label = { Text("Nanos") }
+            )
 
         }
     }
