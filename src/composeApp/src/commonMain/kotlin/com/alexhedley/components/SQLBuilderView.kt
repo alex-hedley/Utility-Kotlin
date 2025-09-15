9084fadc73ae1c2879e3578035cf1d65475504f5
Alex Hedley <79629950+alex-hedley@users.noreply.github.com>
2025-09-15 12:53:40 +0100

    #15 WIP LIKE Builder

--- src/composeApp/src/commonMain/kotlin/com/alexhedley/components/SQLBuilderView.kt
+++ src/composeApp/src/commonMain/kotlin/com/alexhedley/components/SQLBuilderView.kt
@@ -2,13 +2,16 @@ package com.alexhedley.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SQLBuilderView() {
@@ -21,8 +24,23 @@ fun SQLBuilderView() {
         Row() {
             Text("SQL Builder", style = MaterialTheme.typography.titleLarge)
         }
+
+//        Spacer(modifier = Modifier.size(10.dp))
+
+//        Row() {
+//            SQLINClause()
+//        }
+
+//        Spacer(modifier = Modifier.size(10.dp))
+
         Row() {
-            SQLINClause()
+            SQLLikeView()
         }
+
+//        Spacer(modifier = Modifier.size(10.dp))
+
+//        Row() {
+//
+//        }
     }
 }
