1ac79a0d8ee5a62120906a7d7f31410f6fcd6674
Alex Hedley <79629950+alex-hedley@users.noreply.github.com>
2025-09-19 12:00:07 +0100

    Reformat Components view

--- src/composeApp/src/commonMain/kotlin/com/alexhedley/ComponentsView.kt
+++ src/composeApp/src/commonMain/kotlin/com/alexhedley/ComponentsView.kt
@@ -7,19 +7,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.alexhedley.models.Component
 
@@ -41,15 +43,23 @@ fun ComponentsView(components: List<Component>) {
                 items(components.size) { idx ->
                     val component = components[idx];
 
-                    Surface(
-                        color = MaterialTheme.colorScheme.surface,
-                        shape = RoundedCornerShape(4.dp),
-                        modifier = Modifier.fillMaxWidth()
+//                    Surface(
+//                        color = MaterialTheme.colorScheme.surface,
+//                        shape = RoundedCornerShape(4.dp),
+//                        modifier = Modifier.fillMaxWidth()
+//                    ) {
+//                    Box(modifier = Modifier, contentAlignment = Alignment.Center) {
+                    Card(
+                        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface), // contentColorFor()
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth(),
+                        elevation =  CardDefaults.cardElevation(defaultElevation = 8.dp),
                     ) {
                         Row() {
                             IconButton(
                                 onClick = component.onClick,
-                                modifier = Modifier.size(40.dp)
+                                modifier = Modifier.size(40.dp),
                             ) {
                                 Icon(
                                     imageVector = component.icon,
@@ -58,10 +68,14 @@ fun ComponentsView(components: List<Component>) {
                             }
                         }
                         Spacer(modifier = Modifier.height(16.dp))
-                        Row() {
-                            Text(component.name)
+                        Row(
+                            Modifier.padding(start = 12.dp, end = 12.dp)
+                        ) {
+                            Text(component.name, textAlign = TextAlign.Center)
                         }
+                        Spacer(modifier = Modifier.height(16.dp))
                     }
+
                 }
             }
         }
